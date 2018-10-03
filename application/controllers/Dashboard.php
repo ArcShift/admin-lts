4ef326cef9446680acf94279792046ec1365c260
ArcShift <ma.kaafi@yahoo.co.id>
2018-10-03 10:35:10 +0700

    load admin lte

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,5 +1,12 @@
 <?php
 
-class Dashboard {
-  //put your code here
+class Dashboard extends CI_Controller {
+
+  public function index() {
+//    echo site_url();
+//    echo '<br/>';
+//    echo base_url();
+    $this->load->view('admin-container');
+  }
+
 }
